abb30776a15a114d9b375b4d10b4e41e0311ee90
andreapanico12 <andreapanico12@gmail.com>
2025-01-27 14:44:04 +0100

    feat: add layout PostCard component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
 
-function PostCard() {
+
+const PostCard =({post}) => {
+
+
   return (
-    <div>PostCard</div>
+    <div className="card">
+      <img src={post.image} className="card-img-top" alt={post.title} />
+      <div className="card-body">
+        <h5 className="card-title">{post.title}</h5>
+        <p className="card-text">
+          {post.tags.map((tag,index) =>(
+            <span key={index} className="badge bg-warning m-1">{tag}</span>
+          ))}
+        </p>
+      </div>
+      
+    </div>
   )
 }
 

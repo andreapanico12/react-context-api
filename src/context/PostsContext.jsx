67a922ca4f99fc81185389908ce53037d39f9607
andreapanico12 <andreapanico12@gmail.com>
2025-01-27 14:24:50 +0100

    feat: add PostsContext logic which can return as children state posts, posts API, fetchPosts function

--- src/context/PostsContext.jsx
+++ src/context/PostsContext.jsx
@@ -1,10 +1,33 @@
 import { createContext, useEffect, useState } from "react"
+import axios from "axios"
+
+const PostsContext = createContext();
+
+const PostsProvider = ({children}) => {
+  const [posts, setPosts] = useState([]);
+  const postApiUrl = "http://localhost:3000";
 
 
-function PostsContext() {
-  return (
-    <div>PostsContext</div>
+  const fetchPosts = () =>{
+    axios.get(`${postApiUrl}/posts`)
+    .then(res => {
+      setPosts(res.data)
+    })
+    .catch (err => {
+      console.error(`Errore durante il caricamento dei post`, err)
+    })
+  }
+
+  useEffect(()=>{
+    fetchPosts()
+  },[]);
+
+  return(
+    <PostsContext.Provider value={{posts, postApiUrl, fetchPosts}}>
+      {children}
+    </PostsContext.Provider>
   )
+
 }
 
-export default PostsContext
+export {PostsContext, PostsProvider}
